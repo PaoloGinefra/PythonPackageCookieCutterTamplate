4576257d506c8d9bf8456b6536ac9878a111e6eb
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-08-15 14:23:47 +0200

    feat: add post gen hook to automatically set up a repo

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Yolo
+# My CookieCutter python package template
+
+This is a template for my go-to Python package starter.
+
+Integrations:
+
+- Pytest
+- Poetry
+
+To generate the template you can run:
+
+```bash
+pip install pipx
+pipx install cookiecutter
+cookiecutter gh:PaoloGinefra/PythonPackageCookieCutterTamplate
+```
---
+++ hooks/post_gen_project.py
@@ -0,0 +1,22 @@
+import subprocess
+
+remoteOrigin = "{{cookiecutter.remoteOrigin}}"
+
+#Initialize git
+subprocess.run("git init")
+
+#Add all content
+subprocess.run("git add .")
+
+#Make first commit
+subprocess.run("git commit -a -m \"first commit: python package boilerplate from https://github.com/PaoloGinefra/PythonPackageCookieCutterTamplate\"")
+
+if remoteOrigin != "None":
+    #Add remote origin
+    subprocess.run(f"git remote add origin {remoteOrigin}")
+
+    #Ensures main branch
+    subprocess.run("git branch -M main")
+
+    #Push to the gh repo
+    subprocess.run("git push -u origin main")
